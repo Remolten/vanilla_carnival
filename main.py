9c9c3bcb8f542ccbc7a81cddec3f9cab384dfccf
Remington Thurber <remingtonthurber@protonmail.com>
2018-04-11 07:57:23 -0600

    Switched to using mini batches to improve accuracy and training time

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ classes = 20
 
 learning_rate = 0.1
 training_epochs = 3
+batch_size = 290  # Set to maximum size that will run on my Macbook Pro GPU, you can make it higher if you have more GPU memory
 nodes_per_layer = 10
 layers = 3  # Including the input layer
 
+config_limit_gpu_memory = 0.49  # Limits how much GPU memory is used so that the program doesn't crash
+
 train_data_raw = fetch_20newsgroups(subset='train', shuffle=True, remove=('headers', 'footers', 'quotes'))
-                                    # categories=('comp.graphics',))
 test_data_raw = fetch_20newsgroups(subset='test', shuffle=True, remove=('headers', 'footers', 'quotes'))
-                                   # categories=('comp.graphics',))
 
 
 def get_network(input_tensor, weights, biases):
@@ -112,10 +113,14 @@ def process_data(train_data, test_data):
     return total_words, train_input, train_output, test_input, test_output
 
 
+def get_batch(input_data, output_data):
+    for i in range(len(input_data) // batch_size + 1):
+        yield input_data[i * batch_size:i * batch_size + batch_size]
+        yield output_data[i * batch_size:i * batch_size + batch_size]
+
+
 def main():
     total_words, train_input, train_output, test_input, test_output = process_data(train_data_raw, test_data_raw)
-
-    print('Processed input data.')
 
     weights = generate_weights(total_words)
     biases = generate_biases()
@@ -136,8 +141,7 @@ def main():
     init = tf.initialize_all_variables()
 
     config = tf.ConfigProto()
-    # config.gpu_options.allow_growth = True
-    config.gpu_options.per_process_gpu_memory_fraction = 0.15
+    config.gpu_options.per_process_gpu_memory_fraction = config_limit_gpu_memory
 
     # Launch the graph
     with tf.Session(config=config) as session:
@@ -145,10 +149,11 @@ def main():
 
         # Training cycle
         for epoch in range(training_epochs):
-            loss_amount, _ = session.run(fetches=[loss, optimizer], feed_dict={input_tensor: train_input, output_tensor: train_output})
-            print('Epoch: {} loss: {}'.format(epoch, loss_amount))
-
-        print('Finished optimization.')
+            for i in range(len(train_input) // batch_size):
+                train_batch_generator = get_batch(train_input, train_output)
+                loss_amount, _ = session.run(fetches=[loss, optimizer], feed_dict={input_tensor: next(train_batch_generator),
+                                                                                   output_tensor: next(train_batch_generator)})
+                print('Epoch: {} batch: {} loss: {}'.format(epoch, i, loss_amount))
 
         # Test model
         correct_prediction = tf.equal(tf.argmax(prediction, 1), tf.argmax(output_tensor, 1))
